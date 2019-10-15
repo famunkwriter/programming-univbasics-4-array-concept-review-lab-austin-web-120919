8aeb604abe1dcb083169740014bbfc0ba51af836
Bretton B Holmes <famunkwriter@me.com>
2019-10-15 12:30:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,18 +10,20 @@ end
 
 def find_max_value(array)
   x = array[0]
-  array.length.times {|index|}
+  array.length.times {|index|
   if array[index] > x
     x = array[index]
   end
+}
 x
 end
 
 def find_min_value(array)
   x = array[0]
-  array.length.times {|index|}
+  array.length.times {|index|
   if array[index] < x
     x = array[index]
   end
-  x
+}
+x
 end
